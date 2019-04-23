440d4536a6d5b1f2533ec3f005e0d2168f67d2d7
Tien Doan <tiendoan246@gmail.com>
2019-04-22 20:47:41 -0700

    Add git ignore file

--- eCommerce/microservice/Products.Service/Controllers/ProductsController.cs
+++ eCommerce/microservice/Products.Service/Controllers/ProductsController.cs
@@ -1,6 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
+using Products.Service.DataTransferObjects.Commands.Products;
+using Products.Service.Products.Commands;
 using Products.Service.Products.Handlers;
+using Service.Common.Exceptions;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,6 +20,36 @@ namespace Products.Service.Controllers
         public ProductsController(IProductCommandHandlers handler)
         {
             this.handler = handler;
+        }
+
+        [HttpPost]
+        public IActionResult Post(CreateProductCommand cmd)
+        {
+            if (string.IsNullOrWhiteSpace(cmd.Name))
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.Forbidden)
+                {
+                    Content = new StringContent("code must be supplied in the body"),
+                    ReasonPhrase = "Missing product code"
+                };
+                return Json(response);
+            }
+
+            try
+            {
+                var command = new CreateProduct(Guid.NewGuid(), cmd.Name, cmd.Description, cmd.Price);
+                handler.Handle(command);
+
+                return Json(string.Format("http://localhost:8181/api/products/{0}", command.Id));
+            }
+            catch (AggregateNotFoundException)
+            {
+                return NotFound();
+            }
+            catch (AggregateDeletedException)
+            {
+                return Conflict();
+            }
         }
 
         // GET: api/<controller>
